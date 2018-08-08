5db7d6e70123f798b8c9edbc1e3efe025d5f6964
Lokendar <lokendar.si@gmail.com>
2018-08-08 20:02:40 +1000

    Initial commit

---
+++ py/notes.py
@@ -0,0 +1,19 @@
+# mylist = [1,2,3,4,5,6,7]
+#
+# print(mylist)
+# mylist2 = [1,2,3,'hello', 'bye',True,False,['a','b','c']]
+# print(len(mylist2))
+# mylist2.extend(mylist)
+# print(mylist2)
+#
+# mat = [[1,2,3],[4,5,6],[7,8,9]]
+#
+# f_col = [row[0] for row in mat]
+#
+# print(f_col)
+
+########Dictionary########
+
+obj = {'key1':'value1','key2':2,'key3':False,'key4':{'iKey5':[1,2,3,'list']}}
+print(obj)
+print(obj['key4']['iKey5'][3].upper())
